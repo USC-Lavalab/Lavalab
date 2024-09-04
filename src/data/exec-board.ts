6f93e2b20a28c1dfad228e1494414f77829afe1a
Cole Gawin <colegawin@gmail.com>
2024-09-04 15:14:48 -0700

    chore: add advik to e-board

--- src/data/exec-board.ts
+++ src/data/exec-board.ts
@@ -8,6 +8,7 @@ import OperationsIcon from "../../static/icon/Operations-Icon/operations.png";
 import PresidentIcon from "../../static/icon/President-Icon/Highlight.png";
 import RecruitmentIcon from "../../static/icon/Recruitment-Icon/Highlight.png";
 import Aditya from "../../static/img/exec/aditya.jpg";
+import Advik from "../../static/img/exec/advik.jpg";
 import Alex from "../../static/img/exec/alex.jpg";
 import Bryan from "../../static/img/exec/bryan.jpg";
 import Cody from "../../static/img/exec/cody.jpg";
@@ -16,7 +17,6 @@ import Iris from "../../static/img/exec/iris.jpg";
 import Jacqueline from "../../static/img/exec/jacqueline.jpg";
 import Josheta from "../../static/img/exec/josheta.jpg";
 import Riya from "../../static/img/exec/riya.jpg";
-import Zhamaika from "../../static/img/exec/zhamaika.jpg";
 import Zuhair from "../../static/img/exec/zuhair.jpg";
 
 const execBoardData = [
@@ -33,6 +33,13 @@ const execBoardData = [
     imgSrc: Bryan,
     linkedInUrl: "https://www.linkedin.com/in/bryan-phillips12/",
     roleIconSrc: RecruitmentIcon,
+  },
+  {
+    name: "Advik Unni",
+    title: "Director of Industry & Sponsorships",
+    imgSrc: Advik,
+    linkedInUrl: "https://www.linkedin.com/in/zhamilyamussaibekova",
+    roleIconSrc: IndustrySponsorshipsIcon,
   },
   {
     name: "Zuhair Lakhani",
